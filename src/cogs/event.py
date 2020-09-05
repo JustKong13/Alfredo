43f49175233c7e9b59cda4838d3e41d4955f0eb1
Justin Kong <64031655+JustKong13@users.noreply.github.com>
2020-09-05 01:33:12 -0400

    added command check
    Fixes bug which may cause Alfredo to respond if a key word is apart of another command

--- src/cogs/event.py
+++ src/cogs/event.py
@@ -9,23 +9,24 @@ class Event(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        with open('events.json') as f:    
-            d = json.load(f)
+        if "$" not in ctx.content:
+            with open('events.json') as f:    
+                d = json.load(f)
 
-            if ctx.author == self.client.user:
-                return
+                if ctx.author == self.client.user:
+                    return
 
-            for event_obj in d["commands"]:
-                if "$list_command" in ctx.content.lower():
-                    break
-                elif ctx.content.lower() == event_obj["input"].lower() or ctx.content.lower().startswith(event_obj["input"].lower() + " ") or ctx.content.lower().endswith(" " + event_obj["input"].lower()) or (" " + event_obj["input"].lower() + " " in ctx.content.lower()):
-                    send = random.choice(event_obj["output"])
-                    if '{}' in send:
-                        await ctx.channel.send(send.format(f'{ctx.author.mention}'))
+                for event_obj in d["commands"]:
+                    if "$list_command" in ctx.content.lower():
+                        break
+                    elif ctx.content.lower() == event_obj["input"].lower() or ctx.content.lower().startswith(event_obj["input"].lower() + " ") or ctx.content.lower().endswith(" " + event_obj["input"].lower()) or (" " + event_obj["input"].lower() + " " in ctx.content.lower()):
+                        send = random.choice(event_obj["output"])
+                        if '{}' in send:
+                            await ctx.channel.send(send.format(f'{ctx.author.mention}'))
+                        else:
+                            await ctx.channel.send(send)
                     else:
-                        await ctx.channel.send(send)
-                else:
-                    pass
+                        pass
 
 
 def setup(client):
