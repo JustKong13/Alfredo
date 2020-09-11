306b39da451d01e70e97eaa8d831374d416e79e9
Justin Kong <64031655+JustKong13@users.noreply.github.com>
2020-09-11 16:04:33 -0400

    Update command.py

--- src/cogs/command.py
+++ src/cogs/command.py
@@ -11,7 +11,7 @@ class Command(commands.Cog):
         self.client = client
         
     @commands.command(name='8ball', help="Ask me a question!")
-    async def _8ball(self, ctx, question):
+    async def _8ball(self, ctx, *, question):
         pred = [
                 "It is certain.",
                 "It is decidedly so.",
@@ -34,12 +34,25 @@ class Command(commands.Cog):
                 "Outlook not so good.",
                 "Very doubtful."
             ]
-        await ctx.channel.send(random.choice(pred))
+        embed_var = discord.Embed(name="8 Ball",
+                                  colour=400597,
+                                  type='rich')
+        embed_var.set_author(name="Alfredo Bot", 
+                             url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=LucasLucas",
+                             icon_url='https://lh3.googleusercontent.com/3cSB32pX5QEEvG6yLLBPyDLOx6814WwGqrOlg8I4PkXeMZcdFrZhb28LGtYRgS_WHqps=s136')
+        embed_var.add_field(name="{}".format(question), value=random.choice(pred), inline=True)
+        embed_var.set_image(url='https://img.favpng.com/7/20/18/magic-8-ball-8-ball-pool-eight-ball-clip-art-png-favpng-pVBbqBupeRZSUSYfJK5E18NGd.jpg')
+        embed_var.set_footer(text="Magic 8 Ball, Wikipedia")
+        await ctx.channel.send(embed=embed_var)
 
     @_8ball.error
     async def _8ball_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.channel.send("Alfredo has nothing to answer. Please provide a question.")
+            embed_var = discord.Embed(colour=400597,
+                                      type='rich')
+            embed_var.add_field(name="ERROR", value="Alfredo has nothing to answer. Please provide a question. inline=True")
+            embed_var.set_image(url='https://img.favpng.com/7/20/18/magic-8-ball-8-ball-pool-eight-ball-clip-art-png-favpng-pVBbqBupeRZSUSYfJK5E18NGd.jpg')
+            embed_var.set_footer(text="Magic 8 Ball, Wikipedia")
 
     @commands.command(aliases=["bae-suzy", 'baesuzy'], help="Random Bae Suzy photo")
     async def bae_suzy(self, ctx):
@@ -47,7 +60,7 @@ class Command(commands.Cog):
         await ctx.channel.send(file=discord.File('bae_suzy/' + str(random.choice(bae_suzy))))
 
     @commands.command(help="View a player's opgg")
-    async def opgg(self, ctx, *,ign):
+    async def opgg(self, ctx, *, ign):
         ign = ign.replace(' ', '%20')
         await ctx.channel.send('https://na.op.gg/summoner/userName='+str(ign)) 
 
@@ -56,15 +69,9 @@ class Command(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.channel.send("Please provide a player IGN.")
 
-    @commands.command()
-    async def tsm(self, ctx):
-        tsm_file = [filename for filename in os.listdir('tsm')]
-        await ctx.channel.send(file=discord.File('tsm/' + str(random.choice(tsm_file))))
-
-    @commands.command()
+    @commands.command(help="Rolls a random number")
     async def roll(self, ctx, i, j):
-        i = int(i)
-        j = int(j)
+        i, j = int(i), int(j)
         value = random.randint(i, j)
         await ctx.channel.send(str(value))
 
@@ -74,30 +81,54 @@ class Command(commands.Cog):
             await ctx.channel.send("Please provide a minimum and maximum number.")
         else:
             await ctx.channel.send("Sorry Alfredo does not understand your number. Please make sure you follow these requirements: \n - Are your numbers integers?\n - Is the first number smaller than your second number? \n - Did you make sure to follow this format: $random <first number> <second number>")
-        
-    @commands.command()
-    async def test(self, ctx):
-        if self.client.latency < 0.08:
-            await ctx.channel.send(f'Alfredo bot is running at {self.client.latency * 1000}ms.')
-        else:
-            await ctx.channel.send(f'Alfredo bot is running at {self.client.latency * 1000}ms. GOD DAMN WE LAGGING LAGGING')
 
-    @commands.command()
-    @commands.has_guild_permissions(manage_messages=True)
-    async def list_command(self, ctx, *, text, help="Lists out Alfredo responses to key words"):
+        
+    @commands.command(help='Check latency of Alfredo')
+    async def test(self, ctx):
+        embed_var = discord.Embed(colour=400597, 
+                                  type='rich')
+        embed_var.set_author(name="Alfredo Bot",
+                             icon_url='https://lh3.googleusercontent.com/3cSB32pX5QEEvG6yLLBPyDLOx6814WwGqrOlg8I4PkXeMZcdFrZhb28LGtYRgS_WHqps=s136')
+        embed_var.add_field(name="Bot is up and running", value=f'Alfredo bot is running at {self.client.latency * 1000}ms.')
+        await ctx.channel.send(embed=embed_var)
+
+    @commands.command(name='list', help="Lists out Alfredo responses to specified key words/phrases")
+    @commands.has_guild_permissions()
+    async def list_command(self, ctx, *, text):
         with open('events.json') as f:
             json_open = json.load(f)
-
+        
         found = False
         for event_dict in json_open['commands']:
             if text == event_dict['input']:
+                found = True
                 response = ""
                 for i in range(len(event_dict['output'])):
-                    response += " " + event_dict['output'][i] + ','
-                await ctx.channel.send(response[:-1])
-                found = True
+                    response += "\n{}. ".format(str(i+1)) + event_dict['output'][i]
+
+                embed_var = discord.Embed(name="Alfredo Responses",
+                                          colour=400597,
+                                          type='rich')
+                embed_var.set_author(name="Alfredo Bot", 
+                                     url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=LucasLucas",
+                                     icon_url='https://lh3.googleusercontent.com/3cSB32pX5QEEvG6yLLBPyDLOx6814WwGqrOlg8I4PkXeMZcdFrZhb28LGtYRgS_WHqps=s136')
+                embed_var.add_field(name="Alfredo's responses to '{}' are:".format(text), value=response, inline=False)
+                await ctx.channel.send(embed=embed_var)
+
         if found == False:
-            await ctx.channel.send('Your command was not found. You can add this command using $add_command {} <output>.'.format(text))
+            response = ""
+            i = 1
+            for event_dict in json_open["commands"]:
+                response += "\n{}. ".format(i) + event_dict['input']
+                i += 1
+            embed_var = discord.Embed(name="Alfredo Responses",
+                                      colour=400597,
+                                      type='rich')
+            embed_var.set_author(name='Alfredo Bot',
+                                 url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=LucasLucas",
+                                 icon_url='https://lh3.googleusercontent.com/3cSB32pX5QEEvG6yLLBPyDLOx6814WwGqrOlg8I4PkXeMZcdFrZhb28LGtYRgS_WHqps=s136')
+            embed_var.add_field(name="Your term was not found. This is what I respond to:", value=response, inline=False) 
+            await ctx.channel.send(embed=embed_var)
 
     @list_command.error
     async def list_command_error(self, ctx, error):
@@ -106,9 +137,18 @@ class Command(commands.Cog):
                 json_open = json.load(f)
             
             response = ""
+            i = 1
             for event_dict in json_open["commands"]:
-                response += "\n - " + event_dict['input'] + ' ' 
-            await ctx.channel.send("This is what I respond to: " + response)
+                response += "\n {}. ".format(str(i)) + event_dict['input']
+                i += 1
+            embed_var = discord.Embed(name="Alfredo Responses",
+                                      colour=400597,
+                                      type='rich')
+            embed_var.set_author(name='Alfredo Bot',
+                                 url="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=LucasLucas",
+                                 icon_url='https://lh3.googleusercontent.com/3cSB32pX5QEEvG6yLLBPyDLOx6814WwGqrOlg8I4PkXeMZcdFrZhb28LGtYRgS_WHqps=s136')
+            embed_var.add_field(name="This is what I respond to: ", value=response)
+            await ctx.channel.send(embed=embed_var)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
